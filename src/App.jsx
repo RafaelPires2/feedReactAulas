0b77fdac87db301996ef1eb8b7d1fa93077ceaf8
rafaelpires2 <rafaelbrpires@gmail.com>
2022-09-27 09:36:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
+
+import styles from './App.module.css';
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+
+      <Header />
+
+      <div className={styles.wrapper}>
+     
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Rafael"
+            content="Bem vindos ao React"      
+          />
+
+          <Post
+            author="Renan"
+            content="Boa noite"      
+          />
+        </main>
+
+      </div>
+    </div>
+  )
+}
+
