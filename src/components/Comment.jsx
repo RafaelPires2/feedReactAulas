4d33eb15f140feec53903e78ecfb49f727a286ec
rafaelpires2 <rafaelbrpires@gmail.com>
2022-09-30 11:58:58 -0300

    function delete comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({content}){
+export function Comment({content, onDeleteComment }){
+  function handleDeleteComment(){
+    console.log('deletar')
+
+    onDeleteComment(content)
+  }
   return (
     <div className={styles.comment}>
       <Avatar src="https://github.com/rafaelpires2.png" />
@@ -15,7 +20,7 @@ export function Comment({content}){
               <time title='26 de setembro de 2022' dateTime='2022-09-26 18:56:00'>Cerca de 2h</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
             <Trash size={24}/>
             </button>
           </header>
