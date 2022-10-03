6bffcac52325f8f2aac1090af6e03aa5c2870c8b
rafaelpires2 <rafaelbrpires@gmail.com>
2022-10-03 10:46:42 -0300

    ref function like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
-
+ 
 export function Comment({content, onDeleteComment }){
   const [likeCount, setLikeCount] = useState(0);
 
@@ -11,7 +11,9 @@ export function Comment({content, onDeleteComment }){
   }
 
   function handleLikeComment(){
-    setLikeCount(likeCount + 1);
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
 
   return (
